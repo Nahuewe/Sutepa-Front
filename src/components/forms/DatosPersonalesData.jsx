b7b9fac1be66eca1cf1bbe1a36998686d5a1cf41
Nahue <nahuelsoriap@gmail.com>
2025-02-18 18:02:03 -0300

    .

--- src/components/forms/DatosPersonalesData.jsx
+++ src/components/forms/DatosPersonalesData.jsx
@@ -219,7 +219,7 @@ function DatosPersonalesData ({ isLoadingParent, register, setValue, errors, wat
 
   useEffect(() => {
     if (activeAfiliado) {
-      const intervals = [500]
+      const intervals = [2000, 6000]
       const timers = intervals.map((interval) =>
         setTimeout(() => {
           setReloadKey((prevKey) => prevKey + 1)
